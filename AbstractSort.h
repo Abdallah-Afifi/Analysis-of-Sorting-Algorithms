42ddefc046b0bad86d5309375ce3e53e2c6ff92d
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-04-11 00:50:43 +0400

    Add files via upload
    added abstract class header and insertion sort header

---
+++ AbstractSort.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class AbstractSort {
+	int size;
+	int* arr;
+public:
+	AbstractSort(int n)
+	{
+		size = n;
+		arr = new int[n];
+	}
+	~AbstractSort()
+	{
+	 delete[] arr;
+	}
+	int getValue(int index)
+	{
+		return arr[index];
+	}
+	int getSize()
+	{
+		return size;
+	}
+	void SetValue(int value, int index)
+	{
+		arr[index] = value;
+	}
+	void PrintArray()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+	int virtual sort(int n, int arr) = 0;
+};
+
---
+++ InsertionSort.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "AbstractSort.h"
+#include <iostream>
+using namespace std;
+
+class InsertionSort : public AbstractSort {
+private:
+public:
+	InsertionSort(int n) : AbstractSort(n) {
+
+	}
+	int sort(int n, int arr)
+	{
+		cout << " 3";
+		return 1;
+	}
+};
+
