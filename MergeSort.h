af79226394cfa754674c5c70535092a471e5f8bb
Adham Gohar <125025049+Adham-Gohar@users.noreply.github.com>
2023-04-12 00:43:10 +0200

    Add files via upload

---
+++ MergeSort.h
@@ -0,0 +1,10 @@
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+class MergeSort
+{
+private:
+	void sort();
+};
+
+#endif 
---
+++ QuickSort.h
@@ -0,0 +1,11 @@
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+class QuickSort
+{
+	enum pivotChoice { First, Random, Median };
+private:
+	void sort();
+};
+
+#endif 
