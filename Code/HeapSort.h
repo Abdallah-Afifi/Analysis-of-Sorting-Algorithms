7582a960b2326c543bda753afb5c5eccc336c474
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-04-19 03:28:16 +0400

    Update HeapSort.h

--- Code/HeapSort.h
+++ Code/HeapSort.h
@@ -4,14 +4,11 @@
 class HeapSort : public SortingAlgorithm {
   
 public:
-HeapSort()
- void Sort(std::vector<int>& vec);
- private:
-  
-
-
-
-
+  HeapSort()
+  void Sort(std::vector<int>& vec);
+private:
+   void BuildMaxHeap(std::vector<int>& vec, int size);
+   void Heapify(std::vector<int>& vec, int i, int size);
 
 
 };
