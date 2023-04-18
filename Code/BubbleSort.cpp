bd84d9339afbf96db31deb597508b6c5ad3c55cc
Abdallah Fathi <114703186+AbdallahFathi22@users.noreply.github.com>
2023-04-19 01:14:05 +0200

    Create BubbleSort.cpp

---
+++ Code/BubbleSort.cpp
@@ -0,0 +1,18 @@
+#include "BubbleSort.h"
+#include "SortingAlgorithm.h"
+#include <vector>
+
+BubbleSort::BubbleSort() : SortingAlgorithm("Bubble Sort") {}
+
+void BubbleSort::Sort(std::vector<int>& vec) {
+    int size = vec.size();
+    for (int i = 0; i < size-1; i++) {
+        for (int j = 0; j < size-i-1; j++) {
+            comparisons++;
+            if (vec[j] > vec[j+1]) {
+                std::swap(vec[j], vec[j+1]);
+                swaps++;
+            }
+        }
+    }
+}
