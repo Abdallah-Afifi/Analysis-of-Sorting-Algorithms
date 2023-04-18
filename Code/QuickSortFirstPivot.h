12ae7c1d543ae4037280c0ccef85e3e96d401865
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-04-19 02:53:43 +0400

    Create QuickSortFirstPivot.h

---
+++ Code/QuickSortFirstPivot.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "SortingAlgorithm.h"
+
+class QuickSortFirstPivot : public SortingAlgorithm {
+public:
+    QuickSortFirstPivot();
+    void Sort(std::vector<int>& vec); 
+
+private:
+    void QuickSortHelper(std::vector<int>& vec, int left, int right); 
+
+    int Partition(std::vector<int>& vec, int left, int right, int pivot_index); 
+};
