c3541cf6a5b7d03291368f42053b25b4e722626e
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-04-19 02:46:54 +0400

    Update InsertionSort.cpp

--- Code/InsertionSort.cpp
+++ Code/InsertionSort.cpp
@@ -7,4 +7,19 @@ using namespace std;
 InsertionSort::InsertionSort() : SortingAlgorithm("Insertion Sort")
 {}
 
-InsertionSort::Sort()
+InsertionSort::Sort(vector<int>& vec) {
+ 
+        int size = arr.size();
+        for (int i = 1; i < size; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+                comparisons++;
+                swaps++;
+            }
+            arr[j + 1] = key;
+        }
+  
+}
