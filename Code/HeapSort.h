2eb3af8364c3f5143ea09beb610e4cf2796559df
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-04-19 03:24:42 +0400

    Create HeapSort.h

---
+++ Code/HeapSort.h
@@ -0,0 +1,17 @@
+#include <vector>
+#include "SortingAlgorithm.h"
+
+class HeapSort : public SortingAlgorithm {
+  
+public:
+HeapSort()
+ void Sort(std::vector<int>& vec);
+ private:
+  
+
+
+
+
+
+
+};
