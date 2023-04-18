499957647efa3e35ed77e17846d4392e4cf8f70e
Abdallah Fathi <114703186+AbdallahFathi22@users.noreply.github.com>
2023-04-19 00:36:58 +0200

    Create SelectionSort.cpp

---
+++ Code/SelectionSort.cpp
@@ -0,0 +1,19 @@
+#include "SelectionSort.h"
+#include <algorithm>
+
+SelectionSort::SelectionSort() : SortingAlgorithm("Selection Sort") {}
+
+void SelectionSort::Sort(vector<int>& vec) {
+    int size = vec.size();
+    for (int i = 0; i < size - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < size; j++) {
+            if (vec[j] < vec[minIdx]) {
+                minIdx = j;
+            }
+            Comparisons++;
+        }
+        swap(vec[i], vec[minIdx]);
+        Swaps++;
+    }
+}
