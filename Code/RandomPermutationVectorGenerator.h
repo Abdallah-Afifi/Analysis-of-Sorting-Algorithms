fe97a0142fd39c6c2f15240010e448d05f74c6e3
Abdallah Fathi <114703186+AbdallahFathi22@users.noreply.github.com>
2023-04-19 01:34:46 +0200

    Create RandomPermutationVectorGenerator.h

---
+++ Code/RandomPermutationVectorGenerator.h
@@ -0,0 +1,12 @@
+#ifndef RANDOM_PERMUTATION_VECTOR_GENERATOR_H
+#define RANDOM_PERMUTATION_VECTOR_GENERATOR_H
+
+#include <vector>
+
+class RandomPermutationVectorGenerator {
+public:
+    static std::vector<int> GenerateVector(int size);
+};
+
+
+#endif 
