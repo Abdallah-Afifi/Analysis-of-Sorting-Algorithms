1db202629eb88e0e4cbf759872ce98ce4328e60f
Abdallah Fathi <114703186+AbdallahFathi22@users.noreply.github.com>
2023-04-19 01:35:24 +0200

    Create RandomPermutationVectorGenerator.cpp

---
+++ Code/RandomPermutationVectorGenerator.cpp
@@ -0,0 +1,14 @@
+#include "RandomPermutationVectorGenerator.h"
+#include <random>
+#include <algorithm>
+
+std::vector<int> RandomPermutationVectorGenerator::GenerateVector(int size) {
+    std::vector<int> vec(size);
+    for (int i = 0; i < size; i++) {
+        vec[i] = i + 1;
+    }
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(vec.begin(), vec.end(), g);
+    return vec;
+}
