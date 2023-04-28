fd5969604d4d604594b4cd965b1c8b5c8eeb1def
Abdallah Fathi <114703186+AbdallahFathi22@users.noreply.github.com>
2023-04-28 02:15:50 +0200

    Update RandomPermutationVectorGenerator.cpp

--- Code/RandomPermutationVectorGenerator.cpp
+++ Code/RandomPermutationVectorGenerator.cpp
@@ -14,3 +14,26 @@ std::vector<int> RandomPermutationVectorGenerator::GenerateVector(int size) {
     std::shuffle(vec.begin(), vec.end(), g);
     return vec;
 }
+
+
+// Function to get the entropy of a vector.
+double GetEntropy(std::vector<int>& vec) {
+    // Get the size of the vector.
+    int size = vec.size();
+    // Create a vector to store the frequencies of each possible value.
+    std::vector<int> frequencies(size, 0);
+    // Iterate over the vector and increment the frequency for each value.
+    for (int i = 0; i < size; i++) {
+        frequencies[vec[i] - 1]++;
+    }
+    // Calculate the entropy.
+    double entropy = 0.0;
+    for (int i = 0; i < size; i++) {
+        if (frequencies[i] > 0) {
+            double p = (double)frequencies[i] / size;
+            entropy -= p * std::log2(p);
+        }
+    }
+    return entropy;
+}
+
